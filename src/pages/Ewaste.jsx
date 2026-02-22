1e52e946d1689660025f6b0a0b7f78107eb2ce9e
Rohith7276 <140833328+Rohith7276@users.noreply.github.com>
2026-02-22 17:05:29 +0530

    final

--- src/pages/Ewaste.jsx
+++ src/pages/Ewaste.jsx
@@ -342,7 +342,13 @@ const StatutoryCompliance = () => {
           <p><span className="font-medium">Importer:</span> SPECULAR GRAPHICS PRIVATE LIMITED, Bangalore.</p>
 
         </Section>
-       
+        <button className="flex m-auto mt-11 ">
+
+          <a href="/axeon.pdf" download="axeon.pdf" className="flex flex-col justify-center items-center gap-2 font-bold">
+            <img src="pdf.png" className="w-32 " alt="download pdf" />
+            Download E-Waste Policy
+          </a>
+        </button>
       </div>
 
     </div >
